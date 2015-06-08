b5c3c7c0f71c9278a9748ed2549c39007fdb5442
arkms <zerodel3000@hotmail.com>
2015-06-08 00:41:25 -0500

    0.9.1.6 Added poolObject
    PoolObject.cs is a class for have a poolmanager in a class, indepent of
    PoolManager,, perfect for you need a pool, but is not necesary all wolrd
    can access it. (BETA!)

---
+++ Assets/Arj2D/Scripts/PoolObject.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ARJ2D
+{
+    public class PoolObject
+    {
+        public List<GameObject> Pool;
+        private bool CanExpandSize;
+        private GameObject Prefab;
+
+        /// <summary>
+        /// Create a pool of one GameObject, indepent of PoolManager
+        /// </summary>
+        /// <param name="_prefab">Prefab</param>
+        /// <param name="_StartSize">Size to start</param>
+        /// <param name="_CanExpandSize">Can create new instances of gameobject, if all are in use? </param>
+        public void Init(GameObject _prefab, int _StartSize, bool _CanExpandSize= true)
+        {
+            if (Pool == null)
+            {
+                Prefab = _prefab;
+                CanExpandSize = _CanExpandSize;
+                Pool = new List<GameObject>();
+                for (int i = 0; i < _StartSize; i++)
+                {
+                    Pool.Add(GameObject.Instantiate(_prefab) as GameObject);
+                    Pool[i].SetActive(false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instantiate a GameObject
+        /// </summary>
+        /// <param name="_position">New position of GameObject</param>
+        /// <returns>The Instantiate of GameObject</returns>
+        public GameObject Spawn(Vector3 _position)
+        {
+            for (int i = 0; i < Pool.Count; i++)
+            {
+                if (!Pool[i].activeSelf)
+                {
+                    Pool[i].SetActive(true);
+                    Pool[i].transform.position = _position;
+                    return Pool[i];
+                }
+            }
+
+            if (CanExpandSize)
+            {
+                GameObject go = GameObject.Instantiate(Prefab) as GameObject;
+                go.transform.position = _position;
+                Pool.Add(go);
+                return go;
+            }
+
+            return null;//is not anyone free
+        }
+
+        /// <summary>
+        /// Instantiate a GameObject
+        /// </summary>
+        /// <param name="_position">New position of GameObject</param>
+        /// <param name="_rotation">New rotation of GameObject</param>
+        /// <returns>The Instantiate of GameObject</returns>
+        public GameObject Spawn(Vector3 _position, Quaternion _rotation)
+        {
+            for (int i = 0; i < Pool.Count; i++)
+            {
+                if (!Pool[i].activeSelf)
+                {
+                    Pool[i].SetActive(true);
+                    Pool[i].transform.position = _position;
+                    Pool[i].transform.rotation = _rotation;
+                    return Pool[i];
+                }
+            }
+
+            if (CanExpandSize)
+            {
+                GameObject go = GameObject.Instantiate(Prefab) as GameObject;
+                go.transform.position = _position;
+                go.transform.rotation = _rotation;
+                Pool.Add(go);
+                return go;
+            }
+
+            return null;//is not anyone free
+        }
+
+        /// <summary>
+        /// Despawn GameObject in pool
+        /// </summary>
+        /// <param name="_gameobject">GameObject to despawn</param>
+        public void Destroy(GameObject _gameobject)
+        {
+            int ID = _gameobject.GetInstanceID();
+
+            for (int i = 0; i < Pool.Count; i++)
+            {
+                if (Pool[i].GetInstanceID() == ID)
+                {
+                    Pool[i].SetActive(false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set if can expand or not if all gameobject in pools are in use
+        /// </summary>
+        /// <param name="_CanExpand">True or false</param>
+        public void Set_Expand(bool _CanExpand)
+        {
+            CanExpandSize = _CanExpand;
+        }
+    }
+}
